ea8c48efc72c0da3ca85e8a0a757cd9a29df77ea
xozgurcanx <petinyanet@gmail.com>
2025-08-28 16:22:10 +0300

    Remove all files

--- assets/js/theme.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const htmlElement = document.documentElement;
-
-  // Always set theme to dark
-  htmlElement.setAttribute("data-theme", "dark");
-  localStorage.setItem("theme", "dark");
-});
